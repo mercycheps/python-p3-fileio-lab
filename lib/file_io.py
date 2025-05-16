c863c0833a6510a6d64d66a6cb38baef76864120
Mercy <maritimmercy8@gmail.com>
2025-05-16 13:56:42 +0300

    Add file I/O functions with write, append, and read

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,14 @@
 def write_file(file_name, file_content):
-    pass
+    full_path = f'{file_name}.txt'
+    with open(full_path, "w") as files:
+        files.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    full_path = f'{file_name}.txt'
+    with open(full_path, "a") as files:
+        files.write(append_content)
 
 def read_file(file_name):
-    pass
+   full_path = f'{file_name}.txt'
+   with open (full_path, "r") as files:
+       return files.read().strip()
